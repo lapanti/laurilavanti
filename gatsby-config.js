ca62cc9c941c7a45dbac314c823dd3508e87b3cc
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2021-09-12 13:16:25 +0300

    feat: add prettier and eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,62 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://laurilavanti.fi",
-    title: "Lauri Lavanti",
-  },
-  plugins: [
-    "gatsby-plugin-preact",
-    "gatsby-plugin-emotion",
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
+    siteMetadata: {
+        siteUrl: 'https://laurilavanti.fi',
+        title: 'Lauri Lavanti',
     },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
-};
+    plugins: [
+        'gatsby-plugin-preact',
+        'gatsby-plugin-emotion',
+        'gatsby-plugin-gatsby-cloud',
+        'gatsby-plugin-image',
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sitemap',
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                icon: 'src/images/icon.png',
+            },
+        },
+        'gatsby-plugin-mdx',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: './src/images/',
+            },
+            __key: 'images',
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'pages',
+                path: './src/pages/',
+            },
+            __key: 'pages',
+        },
+        {
+            resolve: 'gatsby-plugin-prettier-eslint',
+            options: {
+                prettier: {
+                    patterns: [
+                        // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
+                        '**/*.{css,scss,less}',
+                        '**/*.{json,json5}',
+                        '**/*.{graphql}',
+                        '**/*.{md,mdx}',
+                        '**/*.{html}',
+                        '**/*.{yaml,yml}',
+                    ],
+                },
+                eslint: {
+                    patterns: '**/*.{js,jsx,ts,tsx}',
+                    customOptions: {
+                        fix: true,
+                        cache: true,
+                    },
+                },
+            },
+        },
+    ],
+}
