2562cc49e119873a401b90548fa9fbf609c1335c
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2021-09-14 22:03:52 +0300

    feat: style hero image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,30 @@
+import type { PageProps } from 'gatsby'
+import type { ImageDataLike } from 'gatsby-plugin-image'
+
 import { graphql } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import * as React from 'react'
+import tw from 'twin.macro'
 
 import Page from '../components/Page'
 
+const Image = tw(GatsbyImage)`
+    -mt-4.5 object-contain max-w-screen-fullhd w-full h-auto ml-auto mr-auto flex col-span-full
+`
+
+interface Props extends PageProps {
+    data: {
+        heroImg: ImageDataLike
+    }
+}
+
 // markup
-const Index = ({ data }): JSX.Element => {
+const Index = ({ data }: Props): JSX.Element => {
     const image = getImage(data.heroImg)
     return (
         <Page>
             <article>
-                <GatsbyImage image={image} alt="Pyöräilyn harjoittelua tyttäreni kanssa" />
+                {image && <Image image={image} alt="Pyöräilyn harjoittelua tyttäreni kanssa" />}
                 <h1>
                     <dl>
                         <dt>Lauri Lavanti</dt>
