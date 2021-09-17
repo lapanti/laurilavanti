673413d19ec3cf4c5a790036afae5795914aa636
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2021-09-17 23:15:45 +0300

    feat: add redirecting

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk')
 const readingTime = require('reading-time')
 
 exports.onCreateNode = ({ node, actions }) => {
@@ -8,5 +9,37 @@ exports.onCreateNode = ({ node, actions }) => {
             name: `readingTime`,
             value: readingTime(node.rawBody),
         })
+    }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+    const { createRedirect } = actions
+    const result = await graphql(`
+        {
+            allMdx(filter: { frontmatter: { redirect_from: { ne: null } } }) {
+                nodes {
+                    slug
+                    frontmatter {
+                        redirect_from
+                    }
+                }
+            }
+        }
+    `)
+    if (result.errors) {
+        console.log(result.errors)
+    } else {
+        result.data.allMdx.nodes.forEach((post) =>
+            post.frontmatter.redirect_from.forEach((from) =>
+                createRedirect({
+                    fromPath: from,
+                    toPath: post.slug,
+                    isPermanent: true,
+                    redirectInBrowser: true,
+                })
+            )
+        )
+
+        console.log(`${chalk.green('success')} created redirects`)
     }
 }
