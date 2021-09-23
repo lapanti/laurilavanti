d053a4cc319fdb18fc31076044a722407a8debe4
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2021-09-23 09:03:05 +0300

    refactor: fix grid width in over 750 wide screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,7 +35,7 @@ module.exports = {
             gridTemplateColumns: {
                 article: '1fr 0.5rem min(70ch, calc(100% - 1.125rem)) 0.5rem 1fr',
                 article700: '1fr 1.125rem min(70ch, calc(100% - 2.25rem)) 1.125rem 1fr',
-                article750: '1fr 2.25rem min(70ch, calc(100% - 37.875rem)) 2.25rem 1fr',
+                article750: '1fr 2.25rem min(70ch, calc(100% - 4.5rem)) 2.25rem 1fr',
             },
         },
     },
