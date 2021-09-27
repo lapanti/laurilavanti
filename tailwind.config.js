3c439b48204494f00efc5964a34a5bfe40d9a0f5
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2021-09-27 06:16:41 +0300

    refactor: wider content area

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,9 +33,9 @@ module.exports = {
                 layout: '4.5rem 1fr 6.75rem',
             },
             gridTemplateColumns: {
-                article: '1fr 0.5rem min(70ch, calc(100% - 1.125rem)) 0.5rem 1fr',
-                article700: '1fr 1.125rem min(70ch, calc(100% - 2.25rem)) 1.125rem 1fr',
-                article750: '1fr 2.25rem min(70ch, calc(100% - 4.5rem)) 2.25rem 1fr',
+                article: '1fr 0.5rem min(80ch, calc(100% - 1.125rem)) 0.5rem 1fr',
+                article700: '1fr 1.125rem min(80ch, calc(100% - 2.25rem)) 1.125rem 1fr',
+                article750: '1fr 2.25rem min(80ch, calc(100% - 4.5rem)) 2.25rem 1fr',
             },
         },
     },
