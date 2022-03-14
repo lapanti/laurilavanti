5f5f2fba8b04ec40df28a9839521fae16300aaa7
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2022-03-14 14:13:55 +0200

    test: add test for ExternalLink

---
+++ src/components/ExternalLink.spec.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+
+import ExternalLink from './ExternalLink'
+
+describe('<ExternalLink />', () => {
+    it('should render', () => {
+        const href = 'https://github.com/lapanti'
+        const title = 'title'
+        const children = 'children'
+        const { container } = render(
+            <ExternalLink href={href} title={title}>
+                {children}
+            </ExternalLink>
+        )
+
+        const link = screen.getByRole('link', { name: title })
+        expect(link).toHaveAttribute('href', href)
+        expect(link).toHaveAttribute('rel', 'noopener noreferrer')
+        expect(link).toHaveAttribute('target', '_blank')
+
+        expect(container.firstChild).toMatchInlineSnapshot(`
+            <a
+              class="css-1b6i7hm"
+              href="https://github.com/lapanti"
+              rel="noopener noreferrer"
+              target="_blank"
+              title="title"
+            >
+              children
+            </a>
+        `)
+    })
+})
