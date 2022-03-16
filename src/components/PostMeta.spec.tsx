841b1ff1330ebf352a4c03489a57e9d489205f41
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2022-03-16 14:11:20 +0200

    test: even more PostMeta tests

--- src/components/PostMeta.spec.tsx
+++ src/components/PostMeta.spec.tsx
@@ -111,6 +111,107 @@ describe('<PostMeta />', () => {
         `)
     })
 
+    it('should render exactly one minute', () => {
+        const readingTime = MINUTE_IN_MS
+
+        const { container } = render(<PostMeta readingTime={readingTime} tags={tags} date={date} />)
+
+        expect(screen.getByText('1 minuutti')).toBeInTheDocument()
+        expect(screen.getByText(date)).toHaveAttribute('datetime', dateAsDateTime)
+        tags.forEach((tag) =>
+            expect(screen.getByRole('link', { name: `#${tag}` })).toHaveAttribute('href', `/blogi/${tag}`)
+        )
+
+        expect(container.firstChild).toMatchInlineSnapshot(`
+            <aside
+              class="css-j7qwjs"
+            >
+              <div
+                class="css-96di1v"
+              >
+                <span
+                  class="css-1fuc7m2"
+                >
+                  <svg
+                    class="css-79tm4e"
+                  >
+                    <use
+                      xlink:href="#icon-calendar-alt"
+                    />
+                  </svg>
+                  <time
+                    datetime="2022-3-16"
+                  >
+                    16.3.2022
+                  </time>
+                </span>
+                <span
+                  class="css-1fuc7m2"
+                >
+                  <svg
+                    class="css-79tm4e"
+                  >
+                    <use
+                      xlink:href="#icon-clock"
+                    />
+                  </svg>
+                  <span>
+                    1 minuutti
+                  </span>
+                </span>
+              </div>
+              <ul
+                class="css-1xhj18k"
+              >
+                <li
+                  class="css-1rw86pq"
+                >
+                  <a
+                    class="css-1oxnj2p"
+                    href="/blogi/kirkkonummi"
+                  >
+                    #
+                    kirkkonummi
+                  </a>
+                </li>
+                <li
+                  class="css-1rw86pq"
+                >
+                  <a
+                    class="css-1oxnj2p"
+                    href="/blogi/aluevaalit"
+                  >
+                    #
+                    aluevaalit
+                  </a>
+                </li>
+                <li
+                  class="css-1rw86pq"
+                >
+                  <a
+                    class="css-1oxnj2p"
+                    href="/blogi/sote"
+                  >
+                    #
+                    sote
+                  </a>
+                </li>
+                <li
+                  class="css-1rw86pq"
+                >
+                  <a
+                    class="css-1oxnj2p"
+                    href="/blogi/kuntavaalit"
+                  >
+                    #
+                    kuntavaalit
+                  </a>
+                </li>
+              </ul>
+            </aside>
+        `)
+    })
+
     it('should render multiple minutes', () => {
         const minutes = 15
         const readingTime = MINUTE_IN_MS * minutes
