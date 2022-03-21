5d82049bad14621fc4bc9283b2b6d62e646603cd
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2022-03-21 08:39:12 +0200

    test: fix test for 404 page

--- src/pages/404.spec.tsx
+++ src/pages/404.spec.tsx
@@ -3,11 +3,14 @@ import React from 'react'
 
 import FourZeroFour from './404'
 
-describe.skip('<404 />', () => {
+describe('<404 />', () => {
     it('should render', () => {
         const { container } = render(<FourZeroFour />)
 
-        expect(screen.getByText('Etsimääsi sivua ei valitettavasti löytynyt')).toBeInTheDocument()
-        expect(container).toMatchInlineSnapshot()
+        expect(screen.getByText(/Etsimääsi sivua ei valitettavasti löytynyt/i)).toBeInTheDocument()
+
+        expect(screen.getAllByRole('article')).toHaveLength(6)
+
+        expect(container).toMatchSnapshot()
     })
 })
