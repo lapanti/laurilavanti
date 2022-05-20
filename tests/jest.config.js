ef6ab13a2537fd19bc3b3e803fb09133a3f2435c
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2022-05-20 23:21:26 +0300

    chore: update jest

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -5,7 +5,6 @@ const config = {
     moduleNameMapper: {
         '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
         '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/tests/__mocks__/file-mock.js`,
-        '^gatsby-page-utils/(.*)$': 'gatsby-page-utils/dist/$1',
     },
     testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
     transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
