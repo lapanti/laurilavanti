686c25b4a64dec575202da43687aecbe146fb072
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2022-06-20 09:35:47 +0300

    feat: get excerpt list data from contentful

--- src/components/ExcerptList.tsx
+++ src/components/ExcerptList.tsx
@@ -1,4 +1,4 @@
-import type { MdxPost } from '../types/mdx'
+import type { ImageDataLike } from 'gatsby-plugin-image'
 
 import { graphql, useStaticQuery } from 'gatsby'
 import React, { useMemo } from 'react'
@@ -13,31 +13,40 @@ interface Props {
     tag?: string
     currentSlug?: string
 }
+interface ContentfulPost {
+    title: string
+    createdAt: string
+    publishDate?: string | null
+    excerpt: string
+    metadata: {
+        tags: {
+            contentful_id: string
+        }[]
+    }
+    headerImage: ImageDataLike
+    slug: string
+}
 
 const ExcerptListComponent = ({ className, limit, relatedTags, tag, currentSlug }: Props): JSX.Element => {
     const data = useStaticQuery<{
-        allMdx: {
-            nodes: MdxPost[]
+        allContentfulPost: {
+            nodes: ContentfulPost[]
         }
     }>(graphql`
         {
-            allMdx(filter: { frontmatter: { tags: { ne: null } } }, sort: { fields: frontmatter___date, order: DESC }) {
+            allContentfulPost(sort: { fields: publishDate, order: DESC }) {
                 nodes {
-                    frontmatter {
-                        title
-                        date(formatString: "L", locale: "fi")
-                        excerpt
-                        tags
-                        image {
-                            childImageSharp {
-                                gatsbyImageData(placeholder: BLURRED)
-                            }
+                    title
+                    createdAt(formatString: "L", locale: "fi")
+                    publishDate(formatString: "L", locale: "fi")
+                    excerpt
+                    metadata {
+                        tags {
+                            contentful_id
                         }
                     }
-                    fields {
-                        readingTime {
-                            time
-                        }
+                    headerImage {
+                        gatsbyImageData(placeholder: BLURRED)
                     }
                     slug
                 }
@@ -46,11 +55,11 @@ const ExcerptListComponent = ({ className, limit, relatedTags, tag, currentSlug 
     `)
 
     const allNodes = useMemo(() => {
-        const all = data.allMdx.nodes
+        const all = data.allContentfulPost.nodes
         if (relatedTags?.length) {
             return all
                 .filter((curr) => !currentSlug || currentSlug !== curr.slug)
-                .reduce<[MdxPost, number][]>(
+                .reduce<[ContentfulPost, number][]>(
                     (acc, curr) => [
                         ...acc,
                         [
@@ -58,7 +67,7 @@ const ExcerptListComponent = ({ className, limit, relatedTags, tag, currentSlug 
                             // 1 point per matching tag
                             relatedTags.reduce<number>(
                                 (pointsAcc, pointsCurr) =>
-                                    pointsAcc + (curr.frontmatter.tags.includes(pointsCurr) ? 1 : 0),
+                                    pointsAcc + (curr.metadata.tags.includes({ contentful_id: pointsCurr }) ? 1 : 0),
                                 0
                             ),
                         ],
@@ -69,8 +78,8 @@ const ExcerptListComponent = ({ className, limit, relatedTags, tag, currentSlug 
                 .sort((a, b) => {
                     // Same points, sort by date
                     if (a[1] === b[1]) {
-                        const [aDay, aMonth, aYear] = a[0].frontmatter.date.split('.').map((s) => parseInt(s, 10))
-                        const [bDay, bMonth, bYear] = b[0].frontmatter.date.split('.').map((s) => parseInt(s, 10))
+                        const [aDay, aMonth, aYear] = a[0].createdAt.split('.').map((s) => parseInt(s, 10))
+                        const [bDay, bMonth, bYear] = b[0].createdAt.split('.').map((s) => parseInt(s, 10))
                         return new Date(bYear, bMonth - 1, bDay).getTime() - new Date(aYear, aMonth - 1, aDay).getTime()
                     }
                     // Higher points first
@@ -79,18 +88,25 @@ const ExcerptListComponent = ({ className, limit, relatedTags, tag, currentSlug 
                 .map((postWithPoints) => postWithPoints[0])
         }
         if (tag) {
-            return all.filter((post) => post.frontmatter.tags.includes(tag))
+            return all.filter((post) => post.metadata.tags.includes({ contentful_id: tag }))
         }
         return all
-    }, [data.allMdx.nodes, relatedTags, currentSlug, tag])
+    }, [data.allContentfulPost.nodes, relatedTags, currentSlug, tag])
 
     const nodes = useMemo(() => (limit ? allNodes.slice(0, limit) : allNodes), [allNodes, limit])
 
     return (
         <ul className={className}>
             {nodes.map((node) => (
-                <li key={node.frontmatter.date}>
-                    <Excerpt {...node.frontmatter} slug={node.slug} readingTime={node.fields.readingTime.time} />
+                <li key={node.publishDate || node.createdAt}>
+                    <Excerpt
+                        date={node.publishDate || node.createdAt}
+                        slug={node.slug}
+                        excerpt={node.excerpt}
+                        image={node.headerImage}
+                        title={node.title}
+                        tags={node.metadata.tags.map(({ contentful_id }) => contentful_id)}
+                    />
                 </li>
             ))}
         </ul>
