25e5e6e0e304fda1e5222efdcad06319b9baf3c5
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2022-07-11 22:34:35 +0300

    feat: make burger fixed

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -22,7 +22,7 @@ const HeaderComponent = ({ className }: Props): JSX.Element => {
 HeaderComponent.displayName = 'Header'
 
 const Header = tw(HeaderComponent)`
-  flex items-center justify-center box-border h-14 w-14 ml-auto bg-opacity-75 rounded-sm bg-white sticky mt-2 mr-2 top-2 z-50
+  flex items-center justify-center box-border h-14 w-14 ml-auto bg-opacity-75 rounded-sm bg-white fixed mt-2 mr-2 top-2 right-2 z-50
 `
 
 export default Header
