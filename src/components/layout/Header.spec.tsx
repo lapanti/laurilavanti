937fe215e964c864f53415a3dab084b696b4ac0f
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2022-07-14 23:18:22 +0300

    feat: finalize new menu

--- src/components/layout/Header.spec.tsx
+++ src/components/layout/Header.spec.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import React from 'react'
 
 import { mainNav } from '../../../tests/mainNav.mock'
@@ -8,11 +9,45 @@ describe('<Header />', () => {
     it('should render', () => {
         const { container } = render(<Header />)
 
-        expect(screen.getByRole('link', { name: /Lauri Lavanti/i })).toHaveAttribute('href', '/')
+        expect(screen.getByRole('button', { name: /Open navigation menu/i })).toBeEnabled()
+
+        expect(container.firstChild).toMatchSnapshot()
+    })
+
+    it('should render opened menu', async () => {
+        const user = userEvent.setup()
+
+        const { container } = render(<Header />)
+
+        await user.click(screen.getByRole('button', { name: /Open navigation menu/i }))
+
+        expect(screen.getByRole('button', { name: /Close navigation menu/i })).toBeEnabled()
 
         mainNav.links.forEach((link) =>
             expect(screen.getByRole('link', { name: link.title })).toHaveAttribute('href', `/${link.slug}/`)
         )
+
+        expect(container.firstChild).toMatchSnapshot()
+    })
+
+    it('should open and close the menu', async () => {
+        const user = userEvent.setup()
+
+        const { container } = render(<Header />)
+
+        await user.click(screen.getByRole('button', { name: /Open navigation menu/i }))
+
+        const closeButton = screen.getByRole('button', { name: /Close navigation menu/i })
+
+        expect(closeButton).toBeEnabled()
+
+        mainNav.links.forEach((link) =>
+            expect(screen.getByRole('link', { name: link.title })).toHaveAttribute('href', `/${link.slug}/`)
+        )
+
+        await user.click(closeButton)
+
+        expect(screen.queryByRole('link', { name: mainNav.links[0].title })).toBeNull()
 
         expect(container.firstChild).toMatchSnapshot()
     })
---
+++ src/components/layout/header/NavLink.tsx
@@ -0,0 +1,27 @@
+import type { MainNav } from '../../../types/contentful'
+
+import React from 'react'
+import tw from 'twin.macro'
+
+import InternalLink from '../../InternalLink'
+
+type Link = MainNav['links'][number]
+
+interface Props extends Link {
+    className?: string
+}
+
+const NavLinkComponent = ({ className, title, slug }: Props): JSX.Element => (
+    <InternalLink className={className} partiallyActive activeStyle={{ textDecoration: 'underline' }} to={`/${slug}/`}>
+        {title}
+    </InternalLink>
+)
+
+NavLinkComponent.displayName = 'NavLink'
+
+const NavLink = tw(NavLinkComponent)`
+    w-full pl-8 mt-6 first-of-type:mt-0
+    text-xl text-navLink hover:text-accent active:text-accent
+`
+
+export default NavLink
