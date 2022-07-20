d6a9a729ba3ac17fe42ab68b6deb895487ab17f4
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2022-07-20 23:32:15 +0300

    refactor: new home title and text

--- src/components/layout/HomeTitle.spec.tsx
+++ src/components/layout/HomeTitle.spec.tsx
@@ -7,12 +7,7 @@ describe('<HomeTitle />', () => {
     it('should render', () => {
         const { container } = render(<HomeTitle />)
 
-        expect(screen.getByRole('term', { name: /Lauri Lavanti/i })).toBeInTheDocument()
-
-        expect(screen.getByRole('definition', { name: /Isä/i })).toBeInTheDocument()
-        expect(screen.getByRole('definition', { name: /Kirkkonummelainen/i })).toBeInTheDocument()
-        expect(screen.getByRole('definition', { name: /Ohjelmistokehittäjä/i })).toBeInTheDocument()
-        expect(screen.getByRole('definition', { name: /Diplomi-insinööri/i })).toBeInTheDocument()
+        expect(screen.getByRole('heading', { name: /Lauri Lavanti/i })).toBeInTheDocument()
 
         expect(container.firstChild).toMatchSnapshot()
     })
--- src/components/layout/HomeTitle.tsx
+++ src/components/layout/HomeTitle.tsx
@@ -1,32 +1,63 @@
 import React from 'react'
-import tw from 'twin.macro'
+/** @ts-expect-error twin.macro typings are incomplete :/ */
+import tw, { styled } from 'twin.macro'
 
-const List = tw.dl` flex flex-col items-end my-6`
+import Paragraph from '../Paragraph'
 
-const Term = tw.dt` self-start`
-
-interface Props {
-    className?: string
-}
-
-const HomeTitleComponent = ({ className }: Props) => (
-    <div className={className}>
-        <List>
-            <Term aria-label="Lauri Lavanti">
-                <h1>Lauri Lavanti</h1>
-            </Term>
-            <dd aria-label="Isä">Isä</dd>
-            <dd aria-label="Kirkkonummelainen">Kirkkonummelainen</dd>
-            <dd aria-label="Ohjelmistokehittäjä">Ohjelmistokehittäjä</dd>
-            <dd aria-label="Diplomi-insinööri">Diplomi-insinööri</dd>
-        </List>
-    </div>
-)
-
-HomeTitleComponent.displayName = 'HomeTitle'
-
-const HomeTitle = tw(HomeTitleComponent)`
-    text-3xl text-accent font-bold col-start-3
+const TitleContainer = tw.div`
+    grid grid-cols-article 700:grid-cols-article700 750:grid-cols-article750
+    col-span-full -mt-164 h-164 z-40
+    text-6xl font-bold text-lightGrey
+    bg-gradient-to-r from-[rgba(0,0,0,.85)] via-[rgba(0,0,0,.05)]
 `
+
+const H1 = tw.h1`
+    col-start-3 flex flex-col justify-end mb-4 overflow-hidden
+`
+
+const Word = tw.span`
+    flex
+`
+
+const Letter = styled.span([
+    `text-shadow: black 1px 1px 1px`,
+    tw`
+        w-10 flex justify-center
+    `,
+])
+
+const EntryP = tw(Paragraph)`
+    col-start-3 mt-4
+`
+
+/*
+const Rest = tw.span`
+    text-3xl flex items-end
+`*/
+
+const HomeTitle = () => (
+    <>
+        <TitleContainer>
+            <H1 aria-label="Lauri Lavanti">
+                {[
+                    ['L', 'aatu'],
+                    ['A', 'voimuus'],
+                    ['U', 'teliaisuus'],
+                    ['R', 'ehellisyys'],
+                    ['I', 'nhimillisyys'],
+                ].map(([firstLetter /*, rest*/]) => (
+                    <Word key={firstLetter}>
+                        <Letter>{firstLetter}</Letter>
+                        {/*<Rest>{rest}</Rest>*/}
+                    </Word>
+                ))}
+            </H1>
+        </TitleContainer>
+        <EntryP>
+            Kirkkonummelainen paikallispolitiikkaa harrastava diplomi-insinööri. Kahden lapsen isä ja
+            ohjelmistokehittäjä.
+        </EntryP>
+    </>
+)
 
 export default HomeTitle
