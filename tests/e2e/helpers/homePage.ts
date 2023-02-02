a627b4b62cf3610cf79b65010089ea7f4f7f71f0
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2023-02-02 09:21:39 +0200

    test(e2e): add home page e2e test

---
+++ tests/e2e/helpers/homePage.ts
@@ -0,0 +1,25 @@
+import type { Locator, Page } from '@playwright/test'
+
+import { expect } from '@playwright/test'
+
+import { AnyPage } from './anyPage'
+
+export class HomePage extends AnyPage {
+    readonly mainTitle: Locator
+    readonly latestTitle: Locator
+
+    constructor(page: Page) {
+        super(page)
+        this.mainTitle = page.getByRole('heading', { name: /Lauri Lavanti/i })
+        this.latestTitle = page.getByRole('heading', { name: /Uusimmat kirjoitukset/i })
+    }
+
+    async goHome() {
+        await this.page.goto('/')
+    }
+
+    async checkTitles() {
+        await expect(this.mainTitle).toBeVisible()
+        await expect(this.latestTitle).toBeVisible()
+    }
+}
---
+++ tests/e2e/homePage.spec.ts
@@ -0,0 +1,14 @@
+import { test } from '@playwright/test'
+
+import { HomePage } from './helpers/homePage'
+
+test.describe('Home Page', () => {
+    test('should render', async ({ page }) => {
+        const homePage = new HomePage(page)
+        await homePage.goHome()
+
+        await homePage.checkMainNavigation()
+
+        await homePage.checkTitles()
+    })
+})
