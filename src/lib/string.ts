e44114c9bead9c7d4b68a00c5369cb5c2fc9b4e0
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2024-01-26 07:15:22 +0200

    refactor: add null as possible value for getValueOrDefault and use it

--- src/lib/string.ts
+++ src/lib/string.ts
@@ -2,7 +2,7 @@
  * Helper function, that returns value, if it is defined and not an empty string,
  * otherwise returns given default value
  */
-export const getValueOrDefault = (value?: string, defaultValue?: string): string | undefined => {
+export const getValueOrDefault = (value?: string | null, defaultValue?: string): string | undefined => {
     if (!value || value === '') return defaultValue
 
     return value
