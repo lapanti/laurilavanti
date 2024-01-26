1fd4d942a1bc26fba8a1637d26a885bb11242177
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2024-01-26 07:46:25 +0200

    test: less duplication in footerNav mock

--- tests/footerNav.mock.ts
+++ tests/footerNav.mock.ts
@@ -1,42 +1,7 @@
 import type { FooterNav } from '../src/types/contentful'
 
+import { facebookLink, instagramLink, linkedInLink, mastodonLink, threadsLink, twitterLink } from './contactInfo.mock'
+
 export const footerNav: FooterNav = {
-    links: [
-        {
-            contentful_id: '7koYhBntPnW59fOamaISl9',
-            title: 'Facebook',
-            url: 'https://www.facebook.com/laurilavanti',
-            icon: 'facebook',
-        },
-        {
-            contentful_id: 'KQpgl5vI3xI6uN6dZd9Gy',
-            title: 'Threads',
-            url: 'https://www.threads.net/@laurilavanti',
-            icon: 'threads',
-        },
-        {
-            contentful_id: '30Ax2dp5Vio9Z8wPfAH6VJ',
-            title: 'Instagram',
-            url: 'https://www.instagram.com/laurilavanti/',
-            icon: 'instagram',
-        },
-        {
-            contentful_id: '74NYC6zhDMIatoVImQBXj0',
-            title: 'LinkedIn',
-            url: 'https://www.linkedin.com/in/lapanti',
-            icon: 'linkedin',
-        },
-        {
-            contentful_id: '3DER2zvkJ1BDyeBCU8aBb3',
-            title: 'Mastodon',
-            url: 'https://mastodontti.fi/@laurilavanti',
-            icon: 'mastodon',
-        },
-        {
-            contentful_id: 'KQpgl5vI3xI6uN6dZd9Gy',
-            title: 'Twitter',
-            url: 'https://twitter.com/laurilavanti',
-            icon: 'twitter',
-        },
-    ],
+    links: [facebookLink, threadsLink, instagramLink, linkedInLink, mastodonLink, twitterLink],
 }
