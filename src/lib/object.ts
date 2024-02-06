fb296920a0b67e201dd6383057ad67684bbb7ec5
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2024-02-06 13:26:24 +0200

    refactor: remove dead code

--- src/lib/object.ts
+++
@@ -1,7 +0,0 @@
-import type { WithNonNullableProperties } from '../types/generic'
-
-export const hasNonNullableProperties = <T, K extends keyof T>(
-    obj: T | null,
-    ...keys: readonly K[]
-): obj is WithNonNullableProperties<T, K> =>
-    obj !== null && obj !== undefined && keys.every((key) => obj[key] !== null && obj[key] !== undefined)
--- src/types/generic.ts
+++
@@ -1,3 +0,0 @@
-export type WithNonNullableProperties<T, K extends keyof T> = T & {
-    [X in K]-?: NonNullable<T[X]>
-}
