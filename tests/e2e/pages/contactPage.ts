91114aa0fa4553c63891a99cd65ee347ba0dc1fa
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2024-06-25 09:50:07 +0300

    test: uncomment bluesky tests

--- tests/e2e/pages/contactPage.ts
+++ tests/e2e/pages/contactPage.ts
@@ -7,7 +7,7 @@ import { AnyPage } from './anyPage'
 export class ContactPage extends AnyPage {
     readonly email: Locator
     readonly facebook: Locator
-    // readonly bluesky: Locator
+    readonly bluesky: Locator
     readonly instagram: Locator
     readonly linkedIn: Locator
     readonly mastodon: Locator
@@ -16,7 +16,7 @@ export class ContactPage extends AnyPage {
         super(page)
         this.email = page.getByText('lauri.lavanti@kirkkonummi.fi')
         this.facebook = page.getByRole('link', { name: /Facebook/i }).first()
-        // this.bluesky = page.getByRole('link', { name: /Bluesky/i }).first()
+        this.bluesky = page.getByRole('link', { name: /Bluesky/i }).first()
         this.instagram = page.getByRole('link', { name: /Instagram/i }).first()
         this.linkedIn = page.getByRole('link', { name: /LinkedIn/i }).first()
         this.mastodon = page.getByRole('link', { name: /Mastodon/i }).first()
@@ -31,7 +31,7 @@ export class ContactPage extends AnyPage {
 
     async checkContent() {
         await expect(this.facebook).toHaveAttribute('href', 'https://www.facebook.com/laurilavanti')
-        // await expect(this.bluesky).toHaveAttribute('href', 'https://bsky.app/profile/laurilavanti.fi')
+        await expect(this.bluesky).toHaveAttribute('href', 'https://bsky.app/profile/laurilavanti.fi')
         await expect(this.instagram).toHaveAttribute('href', 'https://www.instagram.com/laurilavanti/')
         await expect(this.linkedIn).toHaveAttribute('href', 'https://www.linkedin.com/in/lapanti')
         await expect(this.mastodon).toHaveAttribute('href', 'https://mastodontti.fi/@laurilavanti')
