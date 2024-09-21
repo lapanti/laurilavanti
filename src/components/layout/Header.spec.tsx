ab415a2c59dd868fcb9327bddc60db36f4d300b5
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2024-09-21 14:02:51 +0300

    feat: new desktop header

--- src/components/layout/Header.spec.tsx
+++ src/components/layout/Header.spec.tsx
@@ -10,59 +10,11 @@ describe('<Header />', () => {
     it('should render', () => {
         const { container } = render(<Header />)
 
-        expect(screen.getByRole('heading', { name: /Lauri Lavanti/i })).toBeInTheDocument()
-
-        expect(screen.getByRole('button', { name: /Avaa valikko/i })).toBeEnabled()
-
-        expect(container.firstChild).toMatchSnapshot()
-    })
-
-    it('should render front page', () => {
-        const { container } = render(<Header isFrontPage />)
-
-        expect(screen.queryByRole('heading', { name: /Lauri Lavanti/i })).not.toBeInTheDocument()
-
-        expect(screen.getByRole('button', { name: /Avaa valikko/i })).toBeEnabled()
-
-        expect(container.firstChild).toMatchSnapshot()
-    })
-
-    it('should render opened menu', async () => {
-        const user = userEvent.setup()
-
-        const { container } = render(<Header isFrontPage={false} />)
-
-        expect(screen.getByRole('heading', { name: /Lauri Lavanti/i })).toBeInTheDocument()
-
-        await user.click(screen.getByRole('button', { name: /Avaa valikko/i }))
-
-        expect(screen.getByRole('button', { name: /Sulje valikko/i })).toBeEnabled()
+        expect(screen.getByRole('link', { name: /Lauri Lavanti/i })).toHaveAttribute('href', '/')
 
         mainNav.links.forEach((link) =>
             expect(screen.getByRole('link', { name: link.title })).toHaveAttribute('href', `/${link.slug}/`)
         )
-
-        expect(container.firstChild).toMatchSnapshot()
-    })
-
-    it('should open and close the menu', async () => {
-        const user = userEvent.setup()
-
-        const { container } = render(<Header isFrontPage={false} />)
-
-        await user.click(screen.getByRole('button', { name: /Avaa valikko/i }))
-
-        const closeButton = screen.getByRole('button', { name: /Sulje valikko/i })
-
-        expect(closeButton).toBeEnabled()
-
-        mainNav.links.forEach((link) =>
-            expect(screen.getByRole('link', { name: link.title })).toHaveAttribute('href', `/${link.slug}/`)
-        )
-
-        await user.click(closeButton)
-
-        expect(screen.queryByRole('link', { name: mainNav.links[0].title })).toBeNull()
 
         expect(container.firstChild).toMatchSnapshot()
     })
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,102 +1,25 @@
 import type { MainNav } from '../../types/contentful'
 
 import { graphql, useStaticQuery } from 'gatsby'
-import React, { useCallback, useState } from 'react'
-import { FaBars, FaXmark } from 'react-icons/fa6'
+import React from 'react'
 import styled from 'styled-components'
 
-import { colors, shadows, sizes, zIndices } from '../../lib/styles'
-import H2 from '../H2'
+import { colors, sizes, zIndices } from '../../lib/styles'
 import NavLink from './header/NavLink'
 
-const Heading = styled(H2)<{ isVisible?: boolean }>(
-    {
-        padding: sizes[2],
-        backgroundColor: colors.white75,
-        borderRadius: sizes[1.5],
-        borderTopRightRadius: sizes[0.5],
-        borderBottomLeftRadius: sizes[0.5],
-    },
-    ({ isVisible }) => ({
-        visibility: isVisible ? 'visible' : 'hidden',
-    })
-)
-
-const CloseButton = styled.button({
-    cursor: 'pointer',
-    height: sizes[12],
-    width: sizes[12],
+const Half = styled.div({
+    flex: 1,
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: sizes[10],
 })
-
-const OpenButton = styled(CloseButton)<{ $isOpen: boolean }>(
-    {
-        boxSizing: 'border-box',
-        background: colors.white75,
-        borderRadius: sizes[1.5],
-        borderTopLeftRadius: sizes[0.5],
-        borderBottomRightRadius: sizes[0.5],
-        transition: 'all',
-        transitionDuration: '300ms',
-        transitionTimingFunction: 'ease-out',
-    },
-    ({ $isOpen }) => ({
-        visibility: $isOpen ? 'hidden' : 'visible',
-    })
-)
-
-const logoStyles = {
-    height: sizes[8],
-    width: sizes[8],
-}
-
-const Bars = styled(FaBars)(logoStyles)
-
-const Close = styled(FaXmark)(logoStyles)
-
-const CloseBG = styled(CloseButton)<{ $isOpen: boolean }>(
-    {
-        position: 'absolute',
-        top: sizes[0],
-        width: '100vw',
-        height: '100vh',
-        background: colors.transparent,
-    },
-    ({ $isOpen }) => ({
-        visibility: $isOpen ? 'visible' : 'hidden',
-        left: $isOpen ? sizes[0] : '100%',
-    })
-)
-
-const Nav = styled.nav<{ $isOpen: boolean }>(
-    {
-        position: 'fixed',
-        top: sizes[0],
-        width: sizes[72],
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'flex-end',
-        background: colors.white90,
-        transition: 'all',
-        transitionDuration: '300ms',
-        transitionTimingFunction: 'ease-in',
-        boxShadow: shadows.base,
-    },
-    ({ $isOpen }) => ({
-        visibility: $isOpen ? 'visible' : 'hidden',
-        right: $isOpen ? sizes[0] : `-${sizes[80]}`,
-    })
-)
 
 interface Props {
     className?: string
-    isFrontPage?: boolean
 }
 
-const HeaderComponent = ({ className, isFrontPage }: Props): JSX.Element => {
+const HeaderComponent = ({ className }: Props): JSX.Element => {
     const data = useStaticQuery<{ contentfulMainNav: MainNav }>(graphql`
         {
             contentfulMainNav(titleToBeIgnored: { eq: "Main nav" }) {
@@ -108,26 +31,17 @@ const HeaderComponent = ({ className, isFrontPage }: Props): JSX.Element => {
             }
         }
     `)
-    const [isOpen, setIsOpen] = useState<boolean>(false)
-
-    const openMenu = useCallback(() => setIsOpen(true), [])
-    const closeMenu = useCallback(() => setIsOpen(false), [])
 
     return (
         <header className={className}>
-            <OpenButton aria-label="Avaa valikko" onClick={openMenu} $isOpen={isOpen}>
-                <Bars aria-hidden="true" />
-            </OpenButton>
-            <CloseBG onClick={closeMenu} aria-hidden="true" $isOpen={isOpen} />
-            <Nav aria-expanded={isOpen} $isOpen={isOpen}>
-                <CloseButton aria-label="Sulje valikko" onClick={closeMenu}>
-                    <Close aria-hidden="true" />
-                </CloseButton>
+            <Half>
+                <NavLink title="Lauri Lavanti" slug="index" isFrontPage />
+            </Half>
+            <Half>
                 {data.contentfulMainNav.links.map((nav) => (
                     <NavLink {...nav} key={nav.slug} />
                 ))}
-            </Nav>
-            <Heading isVisible={!isFrontPage}>Lauri Lavanti</Heading>
+            </Half>
         </header>
     )
 }
@@ -135,12 +49,12 @@ const HeaderComponent = ({ className, isFrontPage }: Props): JSX.Element => {
 HeaderComponent.displayName = 'Header'
 
 const Header = styled(HeaderComponent)({
+    backgroundColor: colors.evening,
+    color: colors.peach,
     display: 'flex',
-    flexDirection: 'row-reverse',
-    padding: sizes[2],
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: 'row',
     boxSizing: 'border-box',
+    height: sizes[15],
     width: '100%',
     position: 'fixed',
     zIndex: zIndices[50],
