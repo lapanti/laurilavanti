2e648364e37582a7aa0c95e905654caaddd593da
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2024-09-22 08:16:21 +0300

    test: ignore gatsby-ssr in jest

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -7,7 +7,7 @@ const config = {
         '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/tests/__mocks__/file-mock.js`,
     },
     testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`, `tests/e2e`],
-    transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script|gatsby-link)/)`],
+    transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script|gatsby-link|gatsby-ssr)/)`],
     globals: {
         __PATH_PREFIX__: ``,
     },
@@ -40,6 +40,7 @@ const config = {
         '!gatsby-browser.tsx',
         '!gatsby-config.ts',
         '!gatsby-node.js',
+        '!gatsby-ssr.js',
         '!playwright.config.ts',
         '!tests/jest-preprocess.js',
         '!tests/jest.config.js',
