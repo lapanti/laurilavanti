62c41afb7bc767b54e2a8ce3fc93074e724352c1
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2024-09-24 07:18:01 +0300

    test(e2e): stricter screenshots

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,8 +16,8 @@ const config: PlaywrightTestConfig = {
          */
         timeout: 10_000,
         toHaveScreenshot: {
-            threshold: 0.4,
-            maxDiffPixelRatio: 0.4,
+            threshold: 0.2,
+            maxDiffPixelRatio: 0.2,
         },
     },
     /* Run tests in files in parallel */
