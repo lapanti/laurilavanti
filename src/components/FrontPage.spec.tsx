84f473a3c3574b6358469ef9cb3b0a223ca0735a
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2024-09-24 06:44:58 +0300

    refactor: only render headers if text to render

--- src/components/FrontPage.spec.tsx
+++ src/components/FrontPage.spec.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 
+import { mainImage } from '../../tests/images.mock'
 import { frontPage } from '../../tests/pages.mock'
 import FrontPage from './FrontPage'
 
@@ -9,6 +10,31 @@ describe('<FrontPage />', () => {
         const { container } = render(<FrontPage data={{ contentfulPage: frontPage }} pageContext={{ slug: 'index' }} />)
 
         expect(screen.getByRole('heading', { name: frontPage.title })).toBeInTheDocument()
+
+        expect(container).toMatchSnapshot()
+    })
+    it('should render with minimal', () => {
+        const { container } = render(
+            <FrontPage
+                data={{
+                    contentfulPage: {
+                        description: null,
+                        jsonLdType: 'WebSite',
+                        title: '',
+                        secondaryTitle: null,
+                        subtitle: null,
+                        body: {},
+                        image: { localFile: mainImage, description: '' },
+                        mobileImage: { localFile: mainImage, description: '' },
+                        backgroundImage: null,
+                        updatedAt: '',
+                    },
+                }}
+                pageContext={{ slug: 'index' }}
+            />
+        )
+
+        expect(screen.getByRole('heading', { name: '' })).toBeInTheDocument()
 
         expect(container).toMatchSnapshot()
     })
