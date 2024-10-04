639be8b573d23313ee59fa42c6e8301a87cb15fd
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2024-10-04 13:54:20 +0300

    test: add tests for TitleBanner

---
+++ src/components/layout/TitleBanner.spec.tsx
@@ -0,0 +1,88 @@
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+
+import { inFrontOfWoodsImage, inFrontOfWoodsImageDescription } from '../../../tests/images.mock'
+import TitleBanner from './TitleBanner'
+
+describe('<TitleBanner />', () => {
+    const title = 'Banner title'
+    const publishDate = '4.10.2024'
+    const tags = ['kirkkonummi', 'kuntavaalit']
+
+    it('should render minimal', () => {
+        const { container } = render(<TitleBanner title={title} />)
+
+        expect(screen.getByRole('heading', { name: title })).toBeInTheDocument()
+
+        expect(container.firstChild).toMatchSnapshot()
+    })
+
+    it('should render image without alt', () => {
+        const { container } = render(<TitleBanner imageData={inFrontOfWoodsImage} title={title} />)
+
+        expect(screen.getByRole('heading', { name: title })).toBeInTheDocument()
+        // When no alt, image has role "presentation"
+        expect(screen.getByRole('presentation')).toBeInTheDocument()
+
+        expect(container.firstChild).toMatchSnapshot()
+    })
+
+    it('should not render meta, even if present', () => {
+        const { container } = render(
+            <TitleBanner
+                imageAlt={inFrontOfWoodsImageDescription}
+                imageData={inFrontOfWoodsImage}
+                publishDate={publishDate}
+                tags={tags}
+                title={title}
+            />
+        )
+
+        expect(screen.getByRole('heading', { name: title })).toBeInTheDocument()
+        expect(screen.getByRole('img', { name: inFrontOfWoodsImageDescription })).toBeInTheDocument()
+
+        expect(screen.queryByText(publishDate)).toBeNull()
+        tags.forEach((tag) => expect(screen.queryByRole('link', { name: `#${tag}` })).toBeNull())
+
+        expect(container.firstChild).toMatchSnapshot()
+    })
+
+    it('should "render" meta', () => {
+        const { container } = render(
+            <TitleBanner
+                imageAlt={inFrontOfWoodsImageDescription}
+                imageData={inFrontOfWoodsImage}
+                title={title}
+                showMeta
+            />
+        )
+
+        expect(screen.getByRole('heading', { name: title })).toBeInTheDocument()
+        expect(screen.getByRole('img', { name: inFrontOfWoodsImageDescription })).toBeInTheDocument()
+
+        expect(container.firstChild).toMatchSnapshot()
+    })
+
+    it('should render meta', () => {
+        const { container } = render(
+            <TitleBanner
+                imageAlt={inFrontOfWoodsImageDescription}
+                imageData={inFrontOfWoodsImage}
+                publishDate={publishDate}
+                tags={tags}
+                title={title}
+                showMeta
+            />
+        )
+
+        expect(screen.getByRole('heading', { name: title })).toBeInTheDocument()
+        expect(screen.getByRole('img', { name: inFrontOfWoodsImageDescription })).toBeInTheDocument()
+
+        expect(screen.getByText(publishDate)).toBeInTheDocument()
+        tags.forEach((tag) =>
+            expect(screen.getByRole('link', { name: `#${tag}` })).toHaveAttribute('href', `/kategoria/${tag}/`)
+        )
+
+        expect(container.firstChild).toMatchSnapshot()
+    })
+})
