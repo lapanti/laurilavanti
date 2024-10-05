3cead46501a45b2601c77415a124b65e288a32fa
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2024-10-05 15:47:19 +0300

    chore(eslint): sort keys in Paragraph

--- src/components/Paragraph.ts
+++ src/components/Paragraph.ts
@@ -6,8 +6,8 @@ const Paragraph = styled.p({
     ...fontSizes[1.5],
     fontFamily: fontFamilies.sans,
     fontWeight: fontWeights.normal,
-    marginTop: sizes[1],
     marginBottom: sizes[1],
+    marginTop: sizes[1],
 })
 
 export default Paragraph
