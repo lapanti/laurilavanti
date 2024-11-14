7388621acb52c8155f57b701f1df2f20d4e562d3
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2024-11-14 11:24:31 +0200

    test: update mocks

--- tests/mainNav.mock.ts
+++ tests/mainNav.mock.ts
@@ -4,17 +4,17 @@ export const mainNav: MainNav = {
     links: [
         {
             contentful_id: '2GzyFqJWez4SwUMv9xVkom',
-            title: 'Minusta',
+            navigationTitle: 'Minusta',
             slug: 'minusta',
         },
         {
             contentful_id: '2UT2gc4TNdEfpA8DfeLeKi',
-            title: 'Blogi',
+            navigationTitle: 'Blogi',
             slug: 'blogi',
         },
         {
             contentful_id: '6g3NEhieZ1R9DYIqP9lNKB',
-            title: 'Ota yhteyttä',
+            navigationTitle: 'Ota yhteyttä',
             slug: 'ota-yhteytta',
         },
     ],
