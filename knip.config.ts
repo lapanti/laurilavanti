6e683e1ecfcc6b9c9abcc6c713dbac71db17f7cc
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2024-11-27 14:13:23 +0200

    chore: add jest-environment-jsdom back

--- knip.config.ts
+++ knip.config.ts
@@ -1,7 +1,7 @@
 import type { KnipConfig } from 'knip'
 
 const config: KnipConfig = {
-    ignoreDependencies: ['babel-preset-gatsby', 'identity-obj-proxy'],
+    ignoreDependencies: ['babel-preset-gatsby', 'identity-obj-proxy', 'jest-environment-jsdom'],
     jest: {
         config: ['jest.config.{js,ts,mjs,cjs,json}', 'package.json'],
         entry: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
