1c03fd136bca6036cfa25439b919314f0d022bb8
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2024-11-28 09:55:15 +0200

    ci: collect lighthouse from serve command

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,9 @@
 module.exports = {
     ci: {
+        collect: {
+            startServerCommand: 'npm run serve',
+            url: ['http://localhost:9000'],
+        },
         upload: {
             target: 'temporary-public-storage',
         },
