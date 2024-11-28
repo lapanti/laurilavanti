ad03459d5699dfce6639ef00bfc31a8ab493dd79
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2024-11-28 12:30:23 +0200

    ci: proper url to lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
     ci: {
         collect: {
             startServerCommand: 'npm run develop',
-            url: ['http://localhost:9000'],
+            url: ['http://localhost:8000'],
         },
         upload: {
             target: 'temporary-public-storage',
