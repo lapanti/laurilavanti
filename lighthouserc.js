2c4331f7ac2c8073d53894d6fe067411173be3ac
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2024-11-28 13:49:28 +0200

    ci: run lighthouse and e2e against cloudflare

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,9 +1,5 @@
 module.exports = {
     ci: {
-        collect: {
-            startServerCommand: 'npm run develop',
-            url: ['http://localhost:8000'],
-        },
         upload: {
             target: 'temporary-public-storage',
         },
