3729d2dfe2c18b926b4a2db3c762fb934e27564c
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2024-11-29 07:36:29 +0200

    ci: set assert preset to lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,8 @@
 module.exports = {
     ci: {
+        assert: {
+            preset: 'lighthouse:recommended',
+        },
         upload: {
             target: 'temporary-public-storage',
         },
