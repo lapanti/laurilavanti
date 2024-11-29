f5efe14b81b89623e7ad8a284d7674c7436147fc
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2024-11-29 06:28:01 +0200

    ci: reorganize jest config

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,22 +1,4 @@
 const config = {
-    transform: {
-        '^.+\\.[jt]sx?$': `<rootDir>/tests/jest-preprocess.js`,
-    },
-    moduleNameMapper: {
-        '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
-        '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/tests/__mocks__/file-mock.js`,
-    },
-    testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`, `tests/e2e`],
-    transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script|gatsby-link|gatsby-ssr)/)`],
-    globals: {
-        __PATH_PREFIX__: ``,
-    },
-    setupFiles: [`<rootDir>/tests/loadershim.js`],
-    testEnvironment: `jsdom`,
-    setupFilesAfterEnv: ['<rootDir>/tests/setup-test-env.ts'],
-    rootDir: '..',
-    snapshotResolver: '<rootDir>/tests/snapshotResolver.js',
-    coverageProvider: 'v8',
     collectCoverageFrom: [
         '**/*.{js,jsx,ts,tsx}',
         '!**/*.spec.{js,jsx,ts,tsx}',
@@ -41,6 +23,24 @@ const config = {
         '!tests/jest.config.js',
         '!tests/snapshotResolver.js',
     ],
+    coverageProvider: 'v8',
+    globals: {
+        __PATH_PREFIX__: ``,
+    },
+    moduleNameMapper: {
+        '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
+        '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/tests/__mocks__/file-mock.js`,
+    },
+    rootDir: '..',
+    setupFiles: [`<rootDir>/tests/loadershim.js`],
+    setupFilesAfterEnv: ['<rootDir>/tests/setup-test-env.ts'],
+    snapshotResolver: '<rootDir>/tests/snapshotResolver.js',
+    testEnvironment: `jsdom`,
+    testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`, `tests/e2e`],
+    transform: {
+        '^.+\\.[jt]sx?$': `<rootDir>/tests/jest-preprocess.js`,
+    },
+    transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script|gatsby-link|gatsby-ssr)/)`],
 }
 
 module.exports = config
