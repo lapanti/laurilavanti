d89b2ebe02269e049baea22318869b5f6b69ba35
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2024-11-29 07:51:18 +0200

    ci: set assertions rules to lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,6 +1,12 @@
 module.exports = {
     ci: {
         assert: {
+            assertions: {
+                'is-crawlable': 'off',
+                'lcp-lazy-loaded': 'off',
+                'unused-css-rules': 'off',
+                'uses-rel-preconnect': 'off',
+            },
             preset: 'lighthouse:recommended',
         },
         upload: {
