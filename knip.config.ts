3a2514632660d10fb577a0ec8631a385f710c26b
Lauri Lavanti <laurilavanti@gmail.com>
2024-12-24 00:31:45 +0200

    chore: add and remove missing or unused deps

--- knip.config.ts
+++ knip.config.ts
@@ -2,7 +2,7 @@ import type { KnipConfig } from 'knip'
 
 const config: KnipConfig = {
     ignore: ['lighthouserc.js'],
-    ignoreDependencies: ['babel-preset-gatsby', 'identity-obj-proxy', 'jest-environment-jsdom'],
+    ignoreDependencies: ['babel-preset-gatsby', 'identity-obj-proxy', 'jest-environment-jsdom', 'eslint-import-resolver-typescript'],
     jest: {
         config: ['jest.config.{js,ts,mjs,cjs,json}', 'package.json'],
         entry: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
