3291263214492aa842c37ee41bb7fd22b4c1db58
Lauri Lavanti <laurilavanti@gmail.com>
2025-01-28 13:58:02 +0200

    test: improve Header tests

--- src/components/layout/Header.spec.tsx
+++ src/components/layout/Header.spec.tsx
@@ -5,36 +5,17 @@ import { mainNav } from '../../../tests/mainNav.mock'
 import Header from './Header'
 
 describe('<Header />', () => {
-    it('should render closed menu', () => {
+    it('should render', () => {
         const { container } = render(<Header />)
-
-        expect(screen.getByRole('button', { name: /Avaa valikko/i })).toBeEnabled()
-        expect(screen.queryByRole('button', { name: /Sulje valikko/i })).toBeNull()
-
-        expect(screen.getByRole('link', { name: /Lauri Lavanti$/i })).toHaveAttribute('href', '/')
-
-        mainNav.links.forEach((link) =>
-            expect(screen.getByRole('link', { name: link.title })).toHaveAttribute('href', `/${link.slug}/`)
-        )
 
         expect(container.firstChild).toMatchSnapshot()
     })
 
-    it('should render open', async () => {
-        const user = userEvent.setup()
-        const { container } = render(<Header />)
-
-        await user.click(screen.getByRole('button', { name: /Avaa valikko/i }))
-
-        expect(screen.getByRole('button', { name: /Sulje valikko/i })).toBeEnabled()
-        expect(screen.queryByRole('button', { name: /Avaa valikko/i })).toBeNull()
-
-        expect(screen.getByRole('link', { name: /Lauri Lavanti$/i })).toHaveAttribute('href', '/')
+    it('should render all links', () => {
+        render(<Header />)
 
         mainNav.links.forEach((link) =>
-            expect(screen.getByRole('link', { name: link.title })).toHaveAttribute('href', `/${link.slug}/`)
+            expect(screen.getAllByRole('link', { name: link.title }).length).toBeGreaterThanOrEqual(1)
         )
-
-        expect(container.firstChild).toMatchSnapshot()
     })
 })
