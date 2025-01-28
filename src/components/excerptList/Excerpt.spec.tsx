994f1c0b183829d58897ff56711a230139ad7f21
Lauri Lavanti <laurilavanti@gmail.com>
2025-01-28 08:47:06 +0200

    test: update Excerpt tests to a more unit based one

--- src/components/excerptList/Excerpt.spec.tsx
+++ src/components/excerptList/Excerpt.spec.tsx
@@ -4,16 +4,16 @@ import { inFrontOfWoodsImage } from '../../../tests/images.mock'
 import Excerpt from './Excerpt'
 
 describe('<Excerpt />', () => {
-    it('should render', () => {
-        const title = 'Terveys kuuluu kaikille'
-        const date = '19.01.2022'
-        const excerpt =
-            'Poistamalla sosiaali- ja terveydenhuollon asiakasmaksut, säästämme byrokratiassa. Lisäksi pääsemme hoitamaan ongelmia ennen kuin niistä tulee merkittäviä. Ja tärkeimpänä varmistamme, ettei kukaan jää ilman hoitoa taloudellisista syistä.'
-        const tags = ['aluevaalit', 'soteuudistus', 'kirkkonummi']
-        const slug = 'sote-on-hyvinvointiyhteiskunnan-kulmakivi'
-        const imageAlt = 'imageAlt'
+    const title = 'Terveys kuuluu kaikille'
+    const date = '19.01.2022'
+    const excerpt =
+        'Poistamalla sosiaali- ja terveydenhuollon asiakasmaksut, säästämme byrokratiassa. Lisäksi pääsemme hoitamaan ongelmia ennen kuin niistä tulee merkittäviä. Ja tärkeimpänä varmistamme, ettei kukaan jää ilman hoitoa taloudellisista syistä.'
+    const tags = ['aluevaalit', 'soteuudistus', 'kirkkonummi']
+    const slug = 'sote-on-hyvinvointiyhteiskunnan-kulmakivi'
+    const imageAlt = 'imageAlt'
 
-        const { container } = render(
+    const renderHelper = () =>
+        render(
             <Excerpt
                 date={date}
                 excerpt={excerpt}
@@ -25,25 +25,57 @@ describe('<Excerpt />', () => {
             />
         )
 
+    it('should render', () => {
+        const { container } = renderHelper()
+
+        expect(container.firstChild).toMatchSnapshot()
+    })
+
+    it('should render article container', () => {
+        renderHelper()
+
         const article = screen.getByRole('article', { name: title })
         expect(article).toHaveAttribute('itemScope', '')
         expect(article).toHaveAttribute('itemType', 'https://schema.org/CreativeWork')
+    })
 
-        expect(screen.getAllByRole('img', { name: imageAlt }).length).toBeGreaterThanOrEqual(1)
+    it('should render image', () => {
+        renderHelper()
+
+        expect(screen.getByRole('img', { name: imageAlt })).toBeInTheDocument()
+    })
+
+    it('should render title', () => {
+        renderHelper()
 
         expect(screen.getByRole('heading', { name: title })).toHaveAttribute('itemProp', 'headline')
+    })
+
+    it('should render link', () => {
+        renderHelper()
 
         const mainLink = screen.getByRole('link', { name: new RegExp(title, 'i') })
         expect(mainLink).toHaveAttribute('href', `/blogi/${slug}/`)
         expect(mainLink).toHaveAttribute('rel', 'permalink')
+    })
+
+    it('should render all tags', () => {
+        renderHelper()
 
         tags.forEach((tag) => {
             expect(screen.getByRole('link', { name: `#${tag}` })).toHaveAttribute('href', `/kategoria/${tag}/`)
         })
+    })
+
+    it('should render date', () => {
+        renderHelper()
 
         expect(screen.getByText(date)).toBeInTheDocument()
-        expect(screen.getByText(excerpt)).toHaveAttribute('itemProp', 'description')
+    })
 
-        expect(container.firstChild).toMatchSnapshot()
+    it('should render description text', () => {
+        renderHelper()
+
+        expect(screen.getByText(excerpt)).toHaveAttribute('itemProp', 'description')
     })
 })
