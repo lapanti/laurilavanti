57b421c4e76db2b65490b58dfb7b93bed621be18
Lauri Lavanti <laurilavanti@gmail.com>
2025-03-07 23:44:52 +0200

    feat: add fake numbers

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -58,7 +58,7 @@ const config: GatsbyConfig = {
     siteMetadata: {
         author: 'laurilavanti',
         bluesky: 'https://bsky.app/profile/laurilavanti.fi',
-        communalElectionNumber: null,
+        communalElectionNumber: 100,
         description:
             'Kirkkonummen Vihreiden puheenjohtaja, isä, johtava ohjelmistokehittäjä, diplomi-insinööri, Lauri Lavanti.',
         facebook: 'https://www.facebook.com/laurilavanti',
@@ -76,7 +76,7 @@ const config: GatsbyConfig = {
         ],
         linkedIn: 'https://www.linkedin.com/in/lapanti',
         locale: 'fi_FI',
-        regionalElectionNumber: null,
+        regionalElectionNumber: 1000,
         siteUrl: 'https://laurilavanti.fi',
         threads: 'https://www.threads.net/@laurilavanti',
         title: 'Lauri Lavanti',
