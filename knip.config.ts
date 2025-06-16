ef47e33ec8dabf381bd839956b83fcf3343a8c89
Lauri Lavanti <laurilavanti@gmail.com>
2025-06-16 14:45:49 +0300

    chore: fix knip issues

--- knip.config.ts
+++ knip.config.ts
@@ -1,12 +1,8 @@
 import type { KnipConfig } from 'knip'
 
 const config: KnipConfig = {
-    ignoreDependencies: [
-        'babel-preset-gatsby',
-        'identity-obj-proxy',
-        'jest-environment-jsdom',
-        'eslint-import-resolver-typescript',
-    ],
+    ignore: ['gatsby-browser.tsx', 'gatsby-ssr.js'],
+    ignoreDependencies: ['babel-preset-gatsby', 'eslint-import-resolver-typescript', '@fontsource/ibm-plex-sans'],
     jest: {
         config: ['jest.config.{js,ts,mjs,cjs,json}', 'package.json'],
         entry: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
