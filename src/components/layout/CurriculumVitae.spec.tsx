fba32280101848f31b85ec362880183d2e8df88e
Lauri Lavanti <laurilavanti@gmail.com>
2025-11-21 10:22:40 +0200

    refactor: proper use of the en dash

--- src/components/layout/CurriculumVitae.spec.tsx
+++ src/components/layout/CurriculumVitae.spec.tsx
@@ -11,6 +11,23 @@ import {
 import CurriculumVitae, { yearsToString } from './CurriculumVitae'
 
 describe('<CurriculumVitae />', () => {
+    describe('yearsToString', () => {
+        const twentyTwentyFive = 2025
+        const twentyThirty = 2030
+
+        it('should return only the year if they are the same', () => {
+            expect(yearsToString(twentyTwentyFive, twentyTwentyFive)).toBe(`${twentyTwentyFive}`)
+        })
+
+        it('should return start year with dash if no end year', () => {
+            expect(yearsToString(twentyTwentyFive)).toBe(`${twentyTwentyFive}–`)
+        })
+
+        it('should return start to end with dash when they are both present and different', () => {
+            expect(yearsToString(twentyTwentyFive, twentyThirty)).toBe(`${twentyTwentyFive}–${twentyThirty}`)
+        })
+    })
+
     it('should render empty', () => {
         const { container } = render(
             <CurriculumVitae
@@ -48,21 +65,19 @@ describe('<CurriculumVitae />', () => {
 
         fiduciaries.forEach(({ duty, organization, startYear, endYear }) => {
             expect(screen.getByText(duty)).toBeInTheDocument()
-            expect(screen.getByText(`${organization} — ${yearsToString(startYear, endYear)}`)).toBeInTheDocument()
+            expect(screen.getByText(`${organization} ${yearsToString(startYear, endYear)}`)).toBeInTheDocument()
         })
 
         jobExperiences.forEach(({ title, company, location, startYear, endYear }) => {
             expect(screen.getAllByText(title).length).toBeGreaterThan(0)
             expect(
-                screen.getByText(`${company} (${location}) — ${yearsToString(startYear, endYear)}`)
+                screen.getByText(`${company} (${location}) ${yearsToString(startYear, endYear)}`)
             ).toBeInTheDocument()
         })
 
         degrees.forEach(({ degree, school, location, startYear, endYear }) => {
             expect(screen.getByText(degree)).toBeInTheDocument()
-            expect(
-                screen.getByText(`${school} (${location}) — ${yearsToString(startYear, endYear)}`)
-            ).toBeInTheDocument()
+            expect(screen.getByText(`${school} (${location}) ${yearsToString(startYear, endYear)}`)).toBeInTheDocument()
         })
 
         expect(container.firstChild).toMatchSnapshot()
