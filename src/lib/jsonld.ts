8d84913488c598b1e3d7d0755ee0b4995ea706ca
Lauri Lavanti <laurilavanti@gmail.com>
2025-12-22 08:39:29 +0200

    feat: add missing SEO attributes to pages

--- src/lib/jsonld.ts
+++ src/lib/jsonld.ts
@@ -4,5 +4,3 @@ export const WEBPAGE = 'WebPage' as const
 export const WEBSITE = 'WebSite' as const
 
 export const JSON_LD_TYPES = [BLOGPOSTING, PERSON, WEBPAGE, WEBSITE] as const
-
-export type JsonLdType = (typeof JSON_LD_TYPES)[number]
