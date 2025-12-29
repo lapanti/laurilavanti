a566661294ffdf3de578ebe07d0aecef83aad4ca
Lauri Lavanti <laurilavanti@gmail.com>
2025-12-29 15:30:42 +0200

    chore: remove unused mocks

--- tests/__mocks__/gatsby-plugin-image.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const gatsby = jest.requireActual('gatsby-plugin-image')
-
-module.exports = {
-    ...gatsby,
-    StaticImage: jest.fn(({ backgroundColor, objectFit, ...props }) => React.createElement('img', props)),
-}
