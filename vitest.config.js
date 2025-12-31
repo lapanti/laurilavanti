6e386da7cfd6b74e2e12d2cee8a4019149659266
Lauri Lavanti <laurilavanti@gmail.com>
2025-12-31 11:05:47 +0200

    chore: remove nonworking vitest

--- vitest.config.js
+++
@@ -1,6 +0,0 @@
-/// <reference types="vitest/config" />
-import { getViteConfig } from 'astro/config' // eslint-disable-line import/no-unresolved
-
-export default getViteConfig({
-    test: {},
-})
