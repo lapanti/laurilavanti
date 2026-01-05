d0ada6dd016bf2cd85bd95ce208b9bceb1f8a8c3
Lauri Lavanti <laurilavanti@gmail.com>
2026-01-05 15:13:31 +0200

    test(unit): add unit tests for h1

---
+++ src/components/titleBanner/H1.spec.ts
@@ -0,0 +1,29 @@
+import { getByRole } from '@testing-library/dom'
+import { describe, expect, it } from 'vitest'
+
+import { renderAstroComponent } from '../../../tests/helpers'
+import H1 from './H1.astro'
+
+describe('<H1 />', () => {
+    const title = 'Test Title'
+
+    it('should render', async () => {
+        const result = await renderAstroComponent(H1, {
+            slots: {
+                default: title,
+            },
+        })
+
+        expect(result.firstChild).toMatchSnapshot()
+    })
+
+    it('should render title', async () => {
+        const result = await renderAstroComponent(H1, {
+            slots: {
+                default: title,
+            },
+        })
+
+        expect(getByRole(result, 'heading', { level: 1, name: title })).toBeDefined()
+    })
+})
