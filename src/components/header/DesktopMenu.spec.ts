efbb9e3de0837d15beff2d2cdcb35b5c485079aa
Lauri Lavanti <laurilavanti@gmail.com>
2026-01-05 09:46:42 +0200

    test(unit): add unit tests for desktop menu

---
+++ src/components/header/DesktopMenu.spec.ts
@@ -0,0 +1,49 @@
+import { getByRole } from '@testing-library/dom'
+import { describe, expect, it } from 'vitest'
+
+import { renderAstroComponent } from '../../../tests/helpers'
+import DesktopMenu from './DesktopMenu.astro'
+
+// filepath: /home/lapanti/code/laurilavanti/src/components/header/DesktopMenu.spec.ts
+
+describe('<DesktopMenu />', () => {
+    const aboutMeNavigationTitle = 'Laurista'
+    const aboutMeSlug = 'minusta'
+    const blogNavigationTitle = 'Blogi'
+    const blogSlug = 'blogi'
+    const links = [
+        { fields: { navigationTitle: aboutMeNavigationTitle, slug: aboutMeSlug, title: 'Minusta' } },
+        { fields: { navigationTitle: blogNavigationTitle, slug: blogSlug, title: 'Blogi' } },
+    ]
+
+    it('should render', async () => {
+        const result = await renderAstroComponent(DesktopMenu, {
+            props: {
+                links,
+            },
+        })
+
+        expect(result.firstChild).toMatchSnapshot()
+    })
+
+    it('should render the main link', async () => {
+        const result = await renderAstroComponent(DesktopMenu, {
+            props: {
+                links,
+            },
+        })
+
+        expect(getByRole(result, 'link', { name: /Lauri Lavanti/i })).toHaveAttribute('href', '/')
+    })
+
+    it('should render all given links', async () => {
+        const result = await renderAstroComponent(DesktopMenu, {
+            props: {
+                links,
+            },
+        })
+
+        expect(getByRole(result, 'link', { name: aboutMeNavigationTitle })).toHaveAttribute('href', `/${aboutMeSlug}/`)
+        expect(getByRole(result, 'link', { name: blogNavigationTitle })).toHaveAttribute('href', `/${blogSlug}/`)
+    })
+})
