00bffa0d102a432ecc6a84b7f055dee2d319610d
Lauri Lavanti <laurilavanti@gmail.com>
2026-01-09 09:44:36 +0200

    test(e2e): initialize playwright

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,13 @@
 /// <reference types="vitest/config" />
 import { getViteConfig } from 'astro/config' // eslint-disable-line import/no-unresolved
 import path from 'path'
+import { configDefaults } from 'vitest/config'
 
 export default getViteConfig(
     {
         test: {
             environment: 'happy-dom',
+            exclude: [...configDefaults.exclude, 'tests/e2e/**'],
             resolveSnapshotPath: (testPath, snapshotExtension) => {
                 const finalTestPath = testPath.replace('src', `tests${path.sep}__snapshots__`)
 
