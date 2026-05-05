ccd7df7e5d27a7f06e5175f7a4575667118a0eeb
Lauri Lavanti <lauri@laurilavanti.fi>
2026-05-05 11:50:56 +0300

    test(e2e): account for new heading in topics page

--- tests/e2e/pages/topicsSwePage.ts
+++ tests/e2e/pages/topicsSwePage.ts
@@ -21,6 +21,6 @@ export class TopicsSwePage extends AnyPage {
     }
 
     async checkContent() {
-        await expect(this.topicHeadings).toHaveCount(12)
+        await expect(this.topicHeadings).toHaveCount(13)
     }
 }
